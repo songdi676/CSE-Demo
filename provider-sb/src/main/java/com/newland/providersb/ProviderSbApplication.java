ed346046945bdfa24d5ba5226d20fbd90e2223c6
Chandler0591 <feilongcug@qq.com>
2018-03-28 12:12:56 +0800

    init provider-sb

---
+++ provider-sb/src/main/java/com/newland/providersb/ProviderSbApplication.java
@@ -0,0 +1,20 @@
+package com.newland.providersb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProviderSbApplication {
+
+	@RequestMapping(value = "/hello")
+	public String hello() {
+		return "Hello, Jack!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderSbApplication.class, args);
+	}
+}
