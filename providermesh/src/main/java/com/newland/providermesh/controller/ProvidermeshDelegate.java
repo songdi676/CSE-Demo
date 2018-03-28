f2959c8c620445c37acee797fe97ba53b13d5490
Chandler0591 <feilongcug@qq.com>
2018-03-28 09:46:37 +0800

    provider mesh

---
+++ providermesh/src/main/java/com/newland/providermesh/controller/ProvidermeshDelegate.java
@@ -0,0 +1,14 @@
+package com.newland.providermesh.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ProvidermeshDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
